9565289f6c717e9dc6424e12ceedd0d3f26afac9
Kraddak <40320975+Kraddak@users.noreply.github.com>
2025-01-18 13:11:12 +0900

    checkpoint, but I kind of lost myself

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import About from "./components/about";
+import Footer from "./components/footer";
+import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+import Services from "./components/services";
+import Tours from "./components/tours";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Services/>
+      <Tours/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const pageLinks = [
+    {id: 1, link: '#home', text: 'home' },
+    {id: 2, link: '#about', text: 'about' },
+    {id: 3, link: '#services', text: 'services' },
+    {id: 4, link: '#tours', text: 'tours' },
+]
+
+export const socialLinks = [
+    {id: 1, link: '#https://www.twitter.com', icon: 'fab fa-twitter' },
+    {id: 2, link: '#https://www.twitter.com', icon: 'fab fa-facebook' },
+    {id: 3, link: '#https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
