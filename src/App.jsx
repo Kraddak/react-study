66d13cd97ecc04d08ec5b39c217d04629d2fa524
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-07-23 13:51:28 +0900

    start with controlled inputs 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Starter from './tutorial/05-leverage-javascript/starter/List'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Starter from "./tutorial/06-forms/starter/01-controlled-inputs";
 
 function App() {
-
-  return (
-    <Starter class="app"></Starter>
-  )
+  return <Starter class="app"></Starter>;
 }
 
-export default App
+export default App;
