4bfc638dff003b378daea7087293025d180a0ddf
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-07-29 16:16:02 +0900

    owari

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "./tutorial/07-useRef/starter/01-useRef-basics";
+import Starter from "./tutorial/08-custom-hooks/starter/02-fetch-data";
 
 function App() {
   return <Starter class="app"></Starter>;
