bbfd40eac1e27415ec74504bcd67406e623211e4
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-08-12 10:20:51 +0900

    tutorial9 owari

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "./tutorial/08-custom-hooks/starter/02-fetch-data";
+import Starter from "./tutorial/09-context-api/starter/Navbar";
 
 function App() {
   return <Starter class="app"></Starter>;
