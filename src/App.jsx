1a580bab6ff193752061f68dbd53ab9b4b7b91c2
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-08-28 17:29:41 +0900

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
-import Starter from "./tutorial/09-context-api/starter/Navbar";
+import Modal from "./sidebarModal/Modal";
+import Sidebar from "./sidebarModal/Sidebar";
+import Home from "./sidebarModal/Home";
+// import Starter from "./tutorial/09-context-api/starter/Navbar";
 
 function App() {
-  return <Starter class="app"></Starter>;
+  // return <Starter class="app"></Starter>;
+  return (
+    <main>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </main>
+  );
 }
 
 export default App;
