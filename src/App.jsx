dd1548f1a65dbb0b3973c0bb08399bc5fddb8ea5
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-10-30 16:47:53 +0900

    02 lower state challenge finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "../src/tutorial/11-performance/starter/01-lower-state";
-// import Starter from "./tutorial/11-performance/starter/01-lower-state.jsx";
+import Starter from "../src/tutorial/11-performance/starter/02-lower-state-challenge";
 
 function App() {
   return <Starter class="app"></Starter>;
