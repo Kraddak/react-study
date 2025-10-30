de7d605b46db85fafa162be0773c846d508d21fe
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-10-30 15:34:05 +0900

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "./tutorial/09-context-api/starter/Navbar";
+import Starter from "./tutorial/10-useReducer/starter/01-useReducer.jsx";
 
 function App() {
   return <Starter class="app"></Starter>;
--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,78 @@
-import React from 'react';
-import { data } from '../../../data';
-const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+import { React, useReducer } from "react";
+import { data } from "../../../data";
 
+const defaultState = {
+  people: data,
+};
+
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case CLEAR_LIST:
+      return { ...state, people: [] };
+    case RESET_LIST:
+      return { ...state, people: data };
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        people: state.people.filter(
+          (person) => person.id !== action.payload.id
+        ),
+      };
+    default:
+      // return state;
+      throw new Error(`No matching ${action.type} - action type`);
+  }
+};
+
+const ReducerBasics = () => {
+  // const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const resetItems = () => {
+    // setPeople(data);
+    dispatch({ type: RESET_LIST });
+  };
+  const clearItems = () => {
+    // setPeople([]);
+    dispatch({ type: CLEAR_LIST });
+  };
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
   };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {state.people.length <= 0 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetItems}
+        >
+          reset items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearItems}
+        >
+          clear items
+        </button>
+      )}
     </div>
   );
 };
