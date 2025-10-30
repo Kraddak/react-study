54f639ed3db194c40594a5d9e52e560b31196bc3
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-10-30 16:13:13 +0900

    01-lower-state finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "./tutorial/10-useReducer/starter/01-useReducer.jsx";
+import Starter from "../src/tutorial/11-performance/starter/01-lower-state";
+// import Starter from "./tutorial/11-performance/starter/01-lower-state.jsx";
 
 function App() {
   return <Starter class="app"></Starter>;
