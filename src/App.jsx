675d361da7c368701a1af9225470f8c538f71121
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-10-31 16:50:48 +0900

    03-hooks finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "../src/tutorial/11-performance/starter/02-lower-state-challenge";
+import Starter from "../src/tutorial/11-performance/starter/03-hooks";
 
 function App() {
   return <Starter class="app"></Starter>;
--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useCallback, useState } from "react";
+import { data } from "../../../../data";
+import List from "./List";
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
 
+  // This function is recreated at every rerendered
+  const removePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const memoizedRemovePerson = useCallback(
+    (id) => {
+      const newPeople = people.filter((person) => person.id !== id);
+      setPeople(newPeople);
+    },
+    [people]
+  );
+
   return (
     <section>
       <button
-        className='btn'
+        className="btn"
         onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
+        style={{ marginBottom: "1rem" }}
       >
         count {count}
       </button>
-      <List people={people} />
+      {/* RemovePerson is recreated => List must rerender! (even if nothing has changed!) */}
+      <List people={people} removePerson={memoizedRemovePerson} />
     </section>
   );
 };
