a47e350062c0d6b3253c2a34f46a6ec7cc95ec46
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-11-04 16:29:01 +0900

    performance finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Starter from "../src/tutorial/11-performance/starter/03-hooks";
+import Starter from "../src/tutorial/11-performance/starter/04-react-18";
 
 function App() {
   return <Starter class="app"></Starter>;
