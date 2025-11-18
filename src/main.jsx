b42515c6528d785729780df52867fd66bfecf4d7
SolinasFran <f.solinas@micronet-techno.co.jp>
2025-11-18 17:21:14 +0900

    start

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// createRoot(document.getElementById("root")).render(<App />);
+import './index.css';
 
-createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </QueryClientProvider>
+  </React.StrictMode>
 );
